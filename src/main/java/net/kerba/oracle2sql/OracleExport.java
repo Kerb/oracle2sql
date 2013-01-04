d4e72ae5d87833b827259f94b18655aef0740906
kerb <fluxoid@gmail.com>
2013-01-05 01:24:24 +0600

    Initial commitk

---
+++ src/main/java/net/kerba/oracle2sql/OracleExport.java
@@ -0,0 +1,13 @@
+package net.kerba.oracle2sql;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kerb
+ * Date: 05.01.13
+ * Time: 1:20
+ */
+public class OracleExport {
+    public static void main(String[] args) {
+
+    }
+}
